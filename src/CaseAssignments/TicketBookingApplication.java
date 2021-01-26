fc9734df27a17f71e91286a2a31d4e379951ab31
Karthik Subramanya <karthiks_27@outlook.com>
2021-01-26 09:35:54 +0530

    Added if loopin Ticket booking application using java

--- src/CaseAssignments/TicketBookingApplication.java
+++ src/CaseAssignments/TicketBookingApplication.java
@@ -12,24 +12,30 @@ public class TicketBookingApplication {
 		// different categories like 3 tickets 1 normal, 1 gold and 1 silver
 
 		int number = 0, ticketpricenormal = 400, ticketpricegold = 200, ticketpricesilver = 150, originalcostnormal = 0,
-				originalcostsilver = 0, originalcostgold = 0, goldclassinputnumber, silverclassinputnumber,
+				originalcostsilver = 0, originalcostgold = 0, goldclassinputnumber = 0, silverclassinputnumber = 0,
 				totalcost = 0;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the world of cinema ");
 		System.out.println("How many tickets do u need ? ");
 		number = sc.nextInt();
-		System.out.println("There are 3 categories of ticket 1. Normal 2.Gold Class and 3.Silver Class");
-		System.out.println("How many Gold class tickets do u need ?");
-		goldclassinputnumber = sc.nextInt();
-		System.out.println("How many Silver class tickets do u need ?");
-		silverclassinputnumber = sc.nextInt();
-
-		if (number == 0 || goldclassinputnumber == 0 || silverclassinputnumber == 0) {
+		if (number == 0 && goldclassinputnumber == 0 && silverclassinputnumber == 0) {
 			System.out.println("Enter at least 1 ticket");
+		} else if (number < 0) {
+			System.out
+					.println("The number entered is not valid, Please enter the valid ticket number between 1..to..5");
 		}
-		if (number > 5) {
+
+		else if (number > 5) {
 			System.out.println("Maximum of 5 tickets are allowed per person");
+			System.out.println("......Thank you visit again.....");
+
+		} else {
+			System.out.println("There are 3 categories of ticket 1. Normal 2.Gold Class and 3.Silver Class");
+			System.out.println("How many Gold class tickets do u need ?");
+			goldclassinputnumber = sc.nextInt();
+			System.out.println("How many Silver class tickets do u need ?");
+			silverclassinputnumber = sc.nextInt();
 		}
 
 		if (goldclassinputnumber > 0 && goldclassinputnumber != 0) {
@@ -42,7 +48,7 @@ public class TicketBookingApplication {
 			System.out.println("You have taken " + silverclassinputnumber
 					+ " silver class ticket, so cost of silver ticket(s) is:" + originalcostsilver);
 		}
-		if (number > 0 || number <= 5) {
+		if (number > 0 && number <= 5) {
 			originalcostnormal = (number - (goldclassinputnumber + silverclassinputnumber)) * ticketpricenormal;
 			totalcost = originalcostnormal + originalcostgold + originalcostsilver;
 
